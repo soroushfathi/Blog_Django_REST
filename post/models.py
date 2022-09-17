b6693de15074f8999fc341c5e8ed891e4016f3df
soroushfathi <soroush8fathi@gmail.com>
2022-09-17 21:31:29 +0430

    Initial commit

---
+++ post/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
