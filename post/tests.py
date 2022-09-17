c3fc1acb5cbc4670dedb198a964319d7a2f9e3b5
soroushfathi <soroush8fathi@gmail.com>
2022-09-17 23:06:58 +0430

    add unit-testing (create post and user)

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+
+class BlogTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user
+        testuser1 = User.objects.create_user(username='test1', password='abc123')
+        testuser1.save()
+
+        # Create a blog post
+        test_post = Post.objects.create(author=testuser1, title='Blog title', body='Content...')
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, 'test1')
+        self.assertEqual(title, 'Blog title')
+        self.assertEqual(body, 'Content...')
