1046708ea4afd7894e41487a741b7eb5585dbc48
soroushfathi <soroush8fathi@gmail.com>
2022-09-18 19:26:44 +0430

    class post serialize

--- post/serializers.py
+++ post/serializers.py
@@ -2,3 +2,8 @@ from rest_framework import serializers
 from .models import Post
 
 
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'title', 'created_at', 'body')
