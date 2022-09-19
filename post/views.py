7bc035773053d0ab406134e8f9dad28f3597caa7
soroushfathi <soroush8fathi@gmail.com>
2022-09-19 09:38:06 +0430

    add permissions

--- post/views.py
+++ post/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import PostSerializer
 from .models import Post
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
