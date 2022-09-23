35957d1545feb180f9e1859353e70e2bb3a8f522
soroushfathi <soroush8fathi@gmail.com>
2022-09-23 15:55:19 +0330

    addd authentication and schema url

--- Blog_REST/urls.py
+++ Blog_REST/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/", include('post.urls')),
+    path("api-auth/", include('rest_framework.urls')),
+    path("api/v1/dj-rest-auth/", include('dj_rest_auth.urls')),
+    path("api/v1/dj-rest-auth/registeration/", include('dj_rest_auth.registration.urls')),
+    path("schema/", get_schema_view(title="Blog API")),
 ]
